a6addab1061729db97804d13ffffb6a2c9d767fc
Dean Musekiwa <dean@lottostar.co.za>
2025-10-15 07:32:51 +0200

    address unused function argument warning

--- src/components/LeagueLoader.tsx
+++ src/components/LeagueLoader.tsx
@@ -1,7 +1,7 @@
 export default function LeagueLoader() {
   return (
     <div className="w-[1240px] max-w-[95%] m-auto grid grid-cols-3 gap-5 max-[1175px]:grid-cols-2 max-[792px]:grid-cols-1">
-      {Array.from({ length: 12 }).map((placeholder, index) => {
+      {Array.from({ length: 12 }).map((_placeholder, index) => {
         return (
           <div key={index} className="bg-slate-950 h-[180px] rounded-lg"></div>
         );
